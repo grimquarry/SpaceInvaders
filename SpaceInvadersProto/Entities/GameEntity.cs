1d9d7a2a114629524635fdec51c729279f43839d
grimquarry <grimquarry@gmail.com>
2019-03-19 14:48:23 -0700

    first commit

---
+++ SpaceInvadersProto/Entities/GameEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace SpaceInvadersProto.Entities
+{
+    //All game entity classes will inherit from the GameEntity class
+    public abstract class GameEntity
+    {
+        //Location of the game entity object
+        public Vector2 Position { get; set; }
+        //Movement speed of the game entity object in pixels per second
+        public Vector2 Velocity { get; set; }
+
+        //Part of the game logic
+        //millisecondsElapsed indicates how much time has passes since this method was last called
+        public virtual void GameTick(float millisecondsElapsed)
+        {
+
+        }
+
+        //Method the game entity object uses to draw itself to the screen
+        public virtual void Draw(WriteableBitmap surface)
+        {
+
+        }
+    }
+}
---
+++ SpaceInvadersProto/World/GameWorld.cs
@@ -0,0 +1,45 @@
+﻿using SpaceInvadersProto.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvadersProto.World
+{
+    public class GameWorld
+    {
+        public GameWorld()
+        {
+            GameEntities = new List<GameEntity>();
+            GameTimer = new Stopwatch();
+        }
+
+        public List<GameEntity> GameEntities { get; }
+        public Stopwatch GameTimer { get; }
+        private TimeSpan previousGameTick;
+
+
+        public void AddEntity(GameEntity entity)
+        {
+            GameEntities.Add(entity);
+        }
+
+        public float EllapsedMillisecondsSinceLastTick
+        {
+            get
+            {
+                return (float)(GameTimer.Elapsed - previousGameTick).TotalMilliseconds;
+            }
+        }
+
+        public void GameTick()
+        {
+            foreach (var entity in GameEntities)
+                entity.GameTick(EllapsedMillisecondsSinceLastTick);
+
+            previousGameTick = GameTimer.Elapsed;
+        }
+    }
+}
